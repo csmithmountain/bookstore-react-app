66142ca6ae1614eb082f8850d811d13ce677c273
csmithmountain <c.savela.smedberg@gmail.com>
2023-07-31 11:40:36 +0200

    New files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Homepage />
     </>
   );
 }
