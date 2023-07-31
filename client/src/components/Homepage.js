8c38fdcd150156eeaf8ccbc979cb65a9fcdea7e3
csmithmountain <c.savela.smedberg@gmail.com>
2023-07-31 12:35:08 +0200

    Some updates

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,3 +1,13 @@
-function Homepage() {}
+import BookList from "./BookList";
+import UserList from "./UserList";
+
+function Homepage() {
+  return (
+    <>
+      <BookList />
+      <UserList />
+    </>
+  );
+}
 
 export default Homepage;
